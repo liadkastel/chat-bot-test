904ff2e44a8c45056464911ac64db3de7c2954a5
Liad <liadka@payoneer.com>
2023-09-13 17:29:25 +0300

    change logic again

--- streamlit_app.py
+++ streamlit_app.py
@@ -28,13 +28,14 @@ with st.sidebar:
 
 if "messages" not in st.session_state:
     st.session_state.messages = []
-    st.session_state.messages.insert(0, {"role": "system", "content": pre_message_to_openai})
+
 
 for message in st.session_state.messages[1:]:
     with st.chat_message(message["role"]):
         st.markdown(message["content"])
 
 if prompt := st.chat_input("What is up?"):
+    st.session_state.messages.insert(0, {"role": "system", "content": pre_message_to_openai})
     st.session_state.messages.append({"role": "user", "content": prompt})
     with st.chat_message("assistant"):
         message_placeholder = st.empty()
