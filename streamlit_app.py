18458f9f72b56cf1ad0263fad893b590f43a716e
Liad <liadka@payoneer.com>
2023-09-13 17:21:19 +0300

    change logic again

--- streamlit_app.py
+++ streamlit_app.py
@@ -35,17 +35,16 @@ for message in st.session_state.messages:
 if prompt := st.chat_input("What is up?"):
     st.session_state.messages.insert(0, {"role": "system", "content": pre_message_to_openai})
     st.session_state.messages.append({"role": "user", "content": prompt})
-    with st.chat_message("user"):
-        st.markdown(prompt)
+  #  with st.chat_message("user"):
+  #      st.markdown(prompt)
     with st.chat_message("assistant"):
         message_placeholder = st.empty()
         full_response = ""
-
         for response in openai.ChatCompletion.create(
             model=MODEL,
-            messages=[{"role": m["role"], "content": m["content"]}
-                      for m in st.session_state.messages], stream=True):
+            messages=[{"role": m["role"], "content": m["content"]} for m in st.session_state.messages], stream=True):
             full_response += response.choices[0].delta.get("content", "")
-            message_placeholder.markdown(full_response + "▌")
+            message_placeholder.markdown(full_response + " ")
         message_placeholder.markdown(full_response)
     st.session_state.messages.append({"role": "assistant", "content": full_response})
+
