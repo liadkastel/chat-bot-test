c590c01137f9e57a44f944d63ed9b5e64c341653
Liad <liadka@payoneer.com>
2023-09-13 22:00:09 +0300

    change logic again

--- streamlit_app.py
+++ streamlit_app.py
@@ -14,25 +14,21 @@ MODEL = "gpt-3.5-turbo"
 
 with st.sidebar:
     st.title('Payoneer email bot')
-    if 'OPENAI_API_KEY' in st.secrets:
-        st.success('API key already provided!', icon='✅')
-        openai.api_key = st.secrets['OPENAI_API_KEY']
-    else:
-        openai.api_key = st.text_input('Enter OpenAI API token:', type='password')
-        if not (openai.api_key.startswith('sk-') and len(openai.api_key) == 51):
-            st.warning('Please enter your credentials!', icon='⚠️')
-        else:
-            st.success('Proceed to entering your prompt message!', icon='👉')
+    st.success('API key already provided!', icon='✅')
+    openai.api_key = st.secrets['OPENAI_API_KEY']
+    if "messages" not in st.session_state:
+        st.session_state.messages = []
+        st.session_state.messages.insert(0, {"role": "system", "content": pre_message_to_openai})
 
-if "messages" not in st.session_state:
-    st.session_state.messages = []
+
+
 
 for message in st.session_state.messages[1:]:
     with st.chat_message(message["role"]):
         st.markdown(message["content"])
 
 if prompt := st.chat_input("What is up?"):
-    st.session_state.messages.insert(0, {"role": "system", "content": pre_message_to_openai})
+
     st.session_state.messages.append({"role": "user", "content": prompt})
     with st.chat_message("assistant"):
         message_placeholder = st.empty()
